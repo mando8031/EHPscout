da64f564fbff6181c60d9c7828a20abc2f146a63
Mando8031 <157807730+mando8031@users.noreply.github.com>
2026-03-13 18:05:08 -0400

    Refactor TBA service to use environment variable for API key

--- src/services/tbaService.js
+++ src/services/tbaService.js
@@ -1,20 +1,29 @@
-import axios from "axios";
+import axios from "axios"
 
-const API_KEY = "PUT_YOUR_TBA_KEY_HERE";
+const API_KEY=process.env.REACT_APP_TBA_KEY
 
-const api = axios.create({
-  baseURL: "https://www.thebluealliance.com/api/v3",
-  headers: {
-    "X-TBA-Auth-Key": API_KEY
+const api=axios.create({
+
+  baseURL:"https://www.thebluealliance.com/api/v3",
+
+  headers:{
+    "X-TBA-Auth-Key":API_KEY
   }
-});
 
-export const getEvents = async (year) => {
-  const res = await api.get(`/events/${year}`);
-  return res.data;
-};
+})
 
-export const getMatches = async (eventKey) => {
-  const res = await api.get(`/event/${eventKey}/matches/simple`);
-  return res.data;
-};
+export const getEvents=async(year)=>{
+
+  const res=await api.get(`/events/${year}`)
+
+  return res.data
+
+}
+
+export const getMatches=async(eventKey)=>{
+
+  const res=await api.get(`/event/${eventKey}/matches/simple`)
+
+  return res.data
+
+}
