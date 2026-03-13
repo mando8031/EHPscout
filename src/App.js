3eee3b3ac3c14c9b31aca17e32872f20cf3a8689
Mando8031 <157807730+mando8031@users.noreply.github.com>
2026-03-13 17:49:55 -0400

    Refactor App component structure and add Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+
 import EventSelect from "./pages/EventSelect";
 import MatchList from "./pages/MatchList";
 import RobotSelect from "./pages/RobotSelect";
@@ -7,24 +9,38 @@ import ScoutForm from "./pages/ScoutForm";
 import Dashboard from "./pages/Dashboard";
 import Picklist from "./pages/Picklist";
 
-function App() {
+function App(){
 
-  return (
+  return(
+
     <BrowserRouter>
 
-      <Routes>
+      <Navbar/>
 
-        <Route path="/" element={<EventSelect />} />
-        <Route path="/matches/:eventKey" element={<MatchList />} />
-        <Route path="/robots" element={<RobotSelect />} />
-        <Route path="/scout" element={<ScoutForm />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/picklist" element={<Picklist />} />
-  
-      </Routes>
+      <div className="p-6">
+
+        <Routes>
+
+          <Route path="/" element={<EventSelect/>} />
+
+          <Route path="/matches/:eventKey" element={<MatchList/>} />
+
+          <Route path="/robots" element={<RobotSelect/>} />
+
+          <Route path="/scout" element={<ScoutForm/>} />
+
+          <Route path="/dashboard" element={<Dashboard/>} />
+
+          <Route path="/picklist" element={<Picklist/>} />
+
+        </Routes>
+
+      </div>
 
     </BrowserRouter>
-  );
+
+  )
+
 }
 
-export default App;
+export default App
