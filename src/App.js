cf201259c32ee37aac9df3d36e78c16efb8d871f
Mando8031 <157807730+mando8031@users.noreply.github.com>
2026-03-13 19:58:16 -0400

    Simplify App component structure
    Removed routing and components for a simplified app structure.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-
-import EventSelect from "./pages/EventSelect";
-import MatchList from "./pages/MatchList";
-import RobotSelect from "./pages/RobotSelect";
-import ScoutForm from "./pages/ScoutForm";
-import Dashboard from "./pages/Dashboard";
-import Picklist from "./pages/Picklist";
-
-function App(){
-
-  return(
-
-    <BrowserRouter>
-
-      <Navbar/>
-
-      <div className="p-6">
-
-        <Routes>
-
-          <Route path="/" element={<EventSelect/>} />
-
-          <Route path="/matches/:eventKey" element={<MatchList/>} />
-
-          <Route path="/robots" element={<RobotSelect/>} />
-
-          <Route path="/scout" element={<ScoutForm/>} />
-
-          <Route path="/dashboard" element={<Dashboard/>} />
-
-          <Route path="/picklist" element={<Picklist/>} />
-
-        </Routes>
-
-      </div>
-
-    </BrowserRouter>
-
-  )
-
+function App() {
+  return (
+    <div>
+      <h1>FRC Scouting App Running</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
