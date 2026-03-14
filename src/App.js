5725b908c4757bc9e96ceaec205516bf23d58b16
Mando8031 <157807730+mando8031@users.noreply.github.com>
2026-03-13 21:00:07 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+import Navbar from "./components/Navbar";
 
-  return (
+import EventSelect from "./pages/EventSelect";
+import MatchList from "./pages/MatchList";
+import RobotSelect from "./pages/RobotSelect";
+import ScoutForm from "./pages/ScoutForm";
+import Dashboard from "./pages/Dashboard";
+import Picklist from "./pages/Picklist";
+
+function App(){
+
+  return(
+
     <BrowserRouter>
-      <div style={{padding:"40px"}}>
-        <h1>FRC Scouting App</h1>
-        <p>If you see this, React is working.</p>
+
+      <Navbar/>
+
+      <div className="p-6">
+
+        <Routes>
+
+          <Route path="/" element={<EventSelect/>} />
+
+          <Route path="/matches/:eventKey" element={<MatchList/>} />
+
+          <Route path="/robots" element={<RobotSelect/>} />
+
+          <Route path="/scout" element={<ScoutForm/>} />
+
+          <Route path="/dashboard" element={<Dashboard/>} />
+
+          <Route path="/picklist" element={<Picklist/>} />
+
+        </Routes>
+
       </div>
+
     </BrowserRouter>
-  );
+
+  )
 
 }
 
-export default App;
+export default App
