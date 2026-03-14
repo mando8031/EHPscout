640ee1540cf88008d3e5eaffa96579f260d48d4d
Mando8031 <157807730+mando8031@users.noreply.github.com>
2026-03-13 21:11:02 -0400

    Update RobotSelect.js

--- src/pages/RobotSelect.js
+++ src/pages/RobotSelect.js
@@ -1,83 +1,14 @@
-const location = useLocation();
-const state = location.state;
+import React from "react";
 
-if(!state){
-  return <div>No match selected</div>;
-}
+function RobotSelect() {
 
-import { useLocation,useNavigate } from "react-router-dom"
-
-function RobotSelect(){
-
-  const {state}=useLocation()
-  const navigate=useNavigate()
-
-  const red=state.alliances.red.team_keys
-  const blue=state.alliances.blue.team_keys
-
-  const select=(team)=>{
-
-    navigate("/scout",{
-      state:{
-        team:team.replace("frc",""),
-        match:state.match_number
-      }
-    })
-
-  }
-
-  return(
-
-    <div>
-
-      <h1 className="text-3xl mb-4">Select Robot</h1>
-
-      <h2 className="text-red-500">Red Alliance</h2>
-
-      {red.map(team=>{
-
-        const num=team.replace("frc","")
-
-        return(
-
-          <div
-          key={team}
-          className="bg-red-700 p-3 rounded mb-2 cursor-pointer"
-          onClick={()=>select(team)}>
-
-            Team {num}
-
-          </div>
-
-        )
-
-      })}
-
-      <h2 className="text-blue-500 mt-6">Blue Alliance</h2>
-
-      {blue.map(team=>{
-
-        const num=team.replace("frc","")
-
-        return(
-
-          <div
-          key={team}
-          className="bg-blue-700 p-3 rounded mb-2 cursor-pointer"
-          onClick={()=>select(team)}>
-
-            Team {num}
-
-          </div>
-
-        )
-
-      })}
-
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Robot Select</h1>
+      <p>Select a team to scout.</p>
     </div>
-
-  )
+  );
 
 }
 
-export default RobotSelect
+export default RobotSelect;
